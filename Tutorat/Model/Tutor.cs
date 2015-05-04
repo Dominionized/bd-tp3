23786f9f85405beaf0214b3047f83b8ea4165163
dominionized <dominique.begin.0@gmail.com>
2015-05-04 13:16:42 -0400

    Added code-first base classes

---
+++ Tutorat/Model/Tutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorat.Model
+{
+    class Tutor
+    {
+        public Tutor() { }
+        [Key]
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public ICollection<TutoringSession> TutoringSessions { get; set; }
+    }
+}
---
+++ Tutorat/Model/TutoringSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorat.Model
+{
+    class TutoringSession
+    {
+        public TutoringSession(){}
+        [Key]
+        public int Id { get; set; }
+        public DateTime DateSession { get; set; }
+        public TimeSpan TimeSession { get; set; }
+        public TimeSpan LengthSession { get; set; }
+
+        public Tutor TutorId { get; set; }
+        public HelpedStudent HelpedId { get; set; }
+    }
+}
