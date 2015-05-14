70292cdf3a616b75ea456fb3733502626428d5e0
dominionized <dominique.begin.0@gmail.com>
2015-05-13 20:01:04 -0400

    Add shahahahahahahahaahhaha

--- Tutorat/Program.cs
+++ Tutorat/Program.cs
@@ -20,26 +20,79 @@ namespace Tutorat
             Database.SetInitializer<TutoringContext>(init);
 
             init.InitializeDatabase(ctx);
-            ctx.Database.Initialize(true);
 
-            var tutoringSessions = ctx.TutoringSessions;
-            foreach (var session in tutoringSessions)
-            {
-                Console.WriteLine(session.ToString());
-            }
+            ListTutors(ctx);
+            ListHelpedStudents(ctx);
+            ListTutoringSessions(ctx);
 
-            var helpedStudents = ctx.HelpedStudents;
-            foreach (var stud in helpedStudents)
-            {
-                Console.WriteLine(stud.ToString());
-            }
+            Console.ReadLine();
+        }
+
+        static void ListTutors(TutoringContext ctx)
+        {
+            Console.WriteLine("Tutors :");
 
             var tutors = ctx.Tutors;
             foreach (var tutor in tutors)
             {
                 Console.WriteLine(tutor.ToString());
             }
-            Console.ReadLine();
+        }
+
+        static void ListHelpedStudents(TutoringContext ctx)
+        {
+            Console.WriteLine("Helped Students :");
+
+            var helpedStudents = ctx.HelpedStudents;
+            foreach (var stud in helpedStudents)
+            {
+                Console.WriteLine(stud.ToString());
+            }
+        }
+
+        static void ListTutoringSessions(TutoringContext ctx)
+        {
+            Console.WriteLine("Tutoring sessions :");
+
+            var tutoringSessions = ctx.TutoringSessions;
+            foreach (var session in tutoringSessions)
+            {
+                Console.WriteLine(session.ToString());
+            }
+        }
+
+        static void executeSectionB(TutoringContext ctx)
+        {
+            // Requete 1
+            var tutors = ctx.Tutors.GroupJoin(ctx.TutoringSessions,
+                    tut => tut.Id,
+                    tutSess => tutSess.TutorId,
+                    (tut, tutSess) => new
+                    {
+                        nom = tut.LastName,
+                        prenom = tut.FirstName,
+                        courriel = tut.EmailAddress,
+                        nbHeures = tutSess.Count()
+                    });
+
+            // Requete 2
+            var tutors = ctx.TutoringSessions
+                .Where(sess => sess.DateSession > DateTime.Now())
+                .Select(s => new {
+                    nom = s.TutorId.LastName,
+                    prenom = s.TutorId.FirstName,
+                    date = s.DateSession,
+                    heure = s.TimeSession,
+                    duree = s.LengthSession,
+                    nom_aide = s.HelpedId.LastName,
+                    prenom_aide = s.HelpedId.FirstName
+                })
+        }
+
+        static void executeSectionC(TutoringContext ctx)
+        {
+            Console.WriteLine("Requete 1");
+
         }
     }
 }
